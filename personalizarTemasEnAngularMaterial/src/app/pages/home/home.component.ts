ac74c7ae44f787f9d8420ec911afdff3c856fc76
Nicolás <nicolas.fernandez.perez.04@gmail.com>
2024-04-14 13:15:27 +0200

    Añadido el proyecto 'Personalizar temas con Angular Material

---
+++ personalizarTemasEnAngularMaterial/src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, MatDividerModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
